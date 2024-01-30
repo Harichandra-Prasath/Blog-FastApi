1dd594f19ba83218f17d945ef5dcaa5990d0cc26
Harichandra-Prasath <1hcp302004@gmail.com>
2024-01-30 20:17:26 +0530

    Dashboard view added

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from src.auth.router import Authrouter
-from src.blog.router import Blogrouter
+from src.blog.router import Blogrouter,Dashboard
+from src.blog.schemas import DashboardResponse
 
 app = FastAPI()
 v1 = FastAPI()
 
 app.mount("/api/v1",v1)
-v1.include_router(router=Authrouter)
-v1.include_router(router=Blogrouter)
+
+v1.add_api_route("/dashboard",Dashboard,response_model=DashboardResponse) 
+v1.include_router(router=Authrouter)      # Auth and User router
+v1.include_router(router=Blogrouter)      # Blogs router
